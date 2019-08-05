d58711b07b2afff37c9709963cea41942a5ff14a
shubh <psubham94@gmail.com>
2019-08-06 00:28:22 +0530

    Completed

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(onRequest);
+
+function onRequest(req,res){
+    var stream = fs.createReadStream('./text.txt');
+    var data = '';
+    var lines = [];
+    stream.on("data",function(chunk){
+    data +=chunk;
+    lines = data.trim().split('\n');
+    
+    lines = lines.slice(lines.length-10);
+    res.write(lines.join('\n'));
+    res.end();
+    });
+}
+server.listen(5000,() => console.log('server listening on port 5K'));
